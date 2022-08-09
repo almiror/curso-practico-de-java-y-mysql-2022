92e7a00b3c14500425fd9859109c615c881c59e8
Julio <almiror.info@gmail.com>
2022-08-09 09:08:39 -0500

    ❤️

--- src/clase01introduccion/Comentarios.java
+++ src/clase01introduccion/Comentarios.java
@@ -3,6 +3,17 @@ package clase01introduccion;
 public class Comentarios {
  
    public static void main(String[] args) {
-        
+     // *****Comentario de Linea*****
+     
+     // System.out.println("No voy a ser ejecutado");
+     
+     
+     /*
+        *****Comentario de Bloque*****
+     
+        System.out.println("No voy a ser ejecutado");
+     
+     */
+       
     }
 }
--- src/clase02sistemadetipos/TiposDeDatosNativos.java
+++ src/clase02sistemadetipos/TiposDeDatosNativos.java
@@ -1,50 +1,50 @@
 package clase02sistemadetipos;
 
 public class TiposDeDatosNativos {
-    // Tipo de dato (char) 'c','h','a','r'
-    // char = "char"
-    // el_tipo_de_dato el_de_la_variable = valor_de_la_varible;
-    // el_tipo_de_dato el_de_la_variable;
-    // es en camel case
-    //cuando declaro una variable en java primero debo de 
-    //iniciar en minúscula luego si sigue otro nombre debe de
-    //iniciar en Mayúscula
+    // TipoDeDato variable = valor;
+    // TipoDeDato variable;
+    /*
+        Las variables en Java Se Declaran en cameCase;
+        Ejemplo:   yoSoyUnaVariableEnCamelCase
+    */
 
     public static void main(String[] args) {
-//        // tipo de dato char
-//        char miChar = 'a';
-//        System.out.println(miChar);
-//
-//        //tipo de dato boolean
-//        boolean esMayor = true;
-//        boolean esMenor = false;
-//        System.out.println(esMayor);
-//        System.out.println(esMenor);
-//        
-//        //tipo de datos int   
-//        int edad = 200;
-//        System.out.println(edad);
-//        
-//        //tipo de dato flotante (float)
-//        float precio = 150f;
-//        System.out.println(precio);
-//        
-//        // tipo de dato decimal
-//        double resultadoConsulta = 10.50;
-//        System.out.println(resultadoConsulta);
+        // tipo de dato char
+        char miChar = 'a';
+        System.out.println(miChar);
+
+        //tipo de dato boolean
+        boolean esMayor = true;
+        boolean esMenor = false;
+        System.out.println(esMayor);
+        System.out.println(esMenor);
+        
+        //tipo de datos int   
+        int edad = 200;
+        System.out.println(edad);
+        
+        //tipo de dato flotante (float)
+        float precio = 150f;
+        System.out.println(precio);
+        
+        // tipo de dato decimal
+        double resultadoConsulta = 10.50;
+        System.out.println(resultadoConsulta);
         
         
         // IMPORTANTE DESDE JAVA 11 EN ADELANTE
-        var nombreVariable = "Hola Mundo";
-        var miChar2 = '5';
-        var edad1 = 50;
-        var monto = 50f;
-        var consulta2 = 120000.5;
+        // Podemos declarar Variables de la palabra reservada (var)
+        var nombreVariable = "Hola Mundo"; // tipo String
+        var miChar2 = 50; // tipo char
+        var edad1 = 50; // tipo int
+        var monto = 50f; // tipo float
+        var consulta2 = 120000.5; // tipo double
         System.out.println(nombreVariable);
         System.out.println(miChar2);
         System.out.println(edad1);
         System.out.println(monto);
         System.out.println(consulta2);
+   
     }
 
 }
