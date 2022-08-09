779be6f6340441c106085431a821c7c36f51f47e
Julio <almiror.info@gmail.com>
2022-08-08 22:44:07 -0500

    PRIMERA CLASE DE JAVA

---
+++ src/clase01introduccion/Comentarios.java
@@ -0,0 +1,8 @@
+package clase01introduccion;
+
+public class Comentarios {
+ 
+   public static void main(String[] args) {
+        
+    }
+}
---
+++ src/clase02sistemadetipos/TiposDeDatosNativos.java
@@ -0,0 +1,50 @@
+package clase02sistemadetipos;
+
+public class TiposDeDatosNativos {
+    // Tipo de dato (char) 'c','h','a','r'
+    // char = "char"
+    // el_tipo_de_dato el_de_la_variable = valor_de_la_varible;
+    // el_tipo_de_dato el_de_la_variable;
+    // es en camel case
+    //cuando declaro una variable en java primero debo de 
+    //iniciar en minúscula luego si sigue otro nombre debe de
+    //iniciar en Mayúscula
+
+    public static void main(String[] args) {
+//        // tipo de dato char
+//        char miChar = 'a';
+//        System.out.println(miChar);
+//
+//        //tipo de dato boolean
+//        boolean esMayor = true;
+//        boolean esMenor = false;
+//        System.out.println(esMayor);
+//        System.out.println(esMenor);
+//        
+//        //tipo de datos int   
+//        int edad = 200;
+//        System.out.println(edad);
+//        
+//        //tipo de dato flotante (float)
+//        float precio = 150f;
+//        System.out.println(precio);
+//        
+//        // tipo de dato decimal
+//        double resultadoConsulta = 10.50;
+//        System.out.println(resultadoConsulta);
+        
+        
+        // IMPORTANTE DESDE JAVA 11 EN ADELANTE
+        var nombreVariable = "Hola Mundo";
+        var miChar2 = '5';
+        var edad1 = 50;
+        var monto = 50f;
+        var consulta2 = 120000.5;
+        System.out.println(nombreVariable);
+        System.out.println(miChar2);
+        System.out.println(edad1);
+        System.out.println(monto);
+        System.out.println(consulta2);
+    }
+
+}
